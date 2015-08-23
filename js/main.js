a906605da71bcc12c8af60c13f27afc5c77c3471
Vladislav <vladonxp@mail.ru>
2015-08-23 12:04:00 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ $(window).load(function(){
 	/* $('.content_box').masonry({
 		itemSelector: '.content_box_menuitem',
 	}); */
- 
+	$('.wrapper').removeClass('loading');
 });
 
 $(document).ready(function(){
 	/* setTimeout(function(){
 		$('.wrapper').removeClass('loading');
 	}, 1500); */
-	$('.wrapper').removeClass('loading');
+	
 })
