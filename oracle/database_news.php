38f7f1674467d7856cfb3361858b6cee0169dec2
vladonxp <vladonxp@mail.ru>
2015-08-28 13:57:47 +0500

    news json right format

--- oracle/database_news.php
+++ oracle/database_news.php
@@ -1,41 +1,35 @@
-<?php
+п»ї<?php
 
-
-   require_once('database_connect.php');
+    require_once('database_connect.php');
+	require_once('../auth/ad_functions.php');
    
-	if (is_null($ID)){
-      	$sql="Select * from news 
-    where ID<=(Select MAX(ID) from news) and ID>=(Select MAX(ID) from news)-10";
+    if (!isset($_POST['id_news'])){
+			$sql="Select * from news 
+						  where ID<=(Select MAX(ID) from news) and ID>=(Select MAX(ID) from news)-10";
+		} else {
+			$sql="Select * from news 
+						  where ID<=".$ID." and ID>=".$ID."-10 ";
+		}
 	
 		$s = OCIParse($c,$sql);
-	OCIExecute($s, OCI_DEFAULT);
-	while(OCIFetch($s)){
-		$ID = ociresult($s,'ID'); //ID новости
-		$NAME_NEWS = ociresult($s,'NAME_NEWS'); //ID новости
-		$DATE_NEWS = ociresult($s,'DATE_NEWS'); //Дата новости
-		$TEXT_NEWS = ociresult($s,'TEXT_NEWS'); //Текст новости
-		$TEXT_NEWS2 = ociresult($s,'TEXT_NEWS2'); //Текст новости2
-		$TEXT_NEWS3 = ociresult($s,'TEXT_NEWS3'); //Текст новости3
-		$TEXT_NEWS4 = ociresult($s,'TEXT_NEWS4'); //Текст новости4
-		$PREV_IMG_NEWS = ociresult($s,'PREV_IMG_NEWS'); //pre image новости
-	}
-		OCICommit($c); 
-	else
-	{
-		$sql="Select * from news 
-    where ID<=".$ID." and ID>=".$ID."-10 ";
+		OCIExecute($s, OCI_DEFAULT);
+		
+	    $news_json = array();
+		$count = 0;
+				
+		while(OCIFetch($s)){
+			
+			$news_json[$count] = array(
+									"id" => ociresult($s,'ID'), 
+									"name_news" => ociresult($s,'NAME_NEWS'), 
+									"date" => ociresult($s,'DATE_NEWS'), 
+									"text" => ociresult($s,'TEXT_NEWS').ociresult($s,'TEXT_NEWS2').ociresult($s,'TEXT_NEWS3').ociresult($s,'TEXT_NEWS4'), 
+									"prev_img" => ociresult($s,'PREV_IMG_NEWS')
+								);
+			$count ++;
+		
+		} 
+		
+		print_r(utf8_json_encode($news_json));		
 	
-		$s = OCIParse($c,$sql);
-	OCIExecute($s, OCI_DEFAULT);
-	while(OCIFetch($s)){
-		$ID = ociresult($s,'ID'); //ID новости
-		$NAME_NEWS = ociresult($s,'NAME_NEWS'); //ID новости
-		$DATE_NEWS = ociresult($s,'DATE_NEWS'); //Дата новости
-		$TEXT_NEWS = ociresult($s,'TEXT_NEWS'); //Текст новости
-		$TEXT_NEWS2 = ociresult($s,'TEXT_NEWS2'); //Текст новости2
-		$TEXT_NEWS3 = ociresult($s,'TEXT_NEWS3'); //Текст новости3
-		$TEXT_NEWS4 = ociresult($s,'TEXT_NEWS4'); //Текст новости4
-		$PREV_IMG_NEWS = ociresult($s,'PREV_IMG_NEWS'); //pre image новости
-	}
-		OCICommit($c); 	
 ?>
