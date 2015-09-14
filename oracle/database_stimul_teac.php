86ea2ce58678de6e471160c0e426d96af953593e
Viking <Viking0607@mail.ru>
2015-09-14 14:11:51 +0500

    Data

--- oracle/database_stimul_teac.php
+++ oracle/database_stimul_teac.php
@@ -5,36 +5,27 @@
    
       $FFIO=$_POST[''];
    
-     $sql="Select * from V_TEACH_APPOINT_ALL 
+     $sql="Select * from v_teac_stimul
 		where instr(
-        upper(replace(replace(FFIO,'.',''),' ','')),
+        upper(replace(replace(FIO,'.',''),' ','')),
         upper(replace(replace('".$FFIO."','.',''),' ','')),1)>=1";
 		
    $s = OCIParse($c,$sql);
 	OCIExecute($s, OCI_DEFAULT);
 	
-		$appoint_teac_json = array();
+		$stimul_teac_json = array();
 	
 				
 		while(OCIFetch($s)){
 			
-			$appoint_teac_json = array(
+			$stimul_teac_json = array(
 									"fio" => ociresult($s,'FFIO'), 
-									"Inst" => ociresult($s,'INSTITUTE'), 
-									"Post" => ociresult($s,'POST'), 
-									"Rate" => ociresult($s,'RATE'), 
-									"Prizn" => ociresult($s,'PRIZN'), 
-									"Bud" => ociresult($s,'BUDGET'), 
-									"Tarif" => ociresult($s,'TARIF'), 
-									"Ncat" => ociresult($s,'CATEGORY'), 
-									"Reg" => ociresult($s,'DATEREG'), 
-									"Sex" => ociresult($s,'SEX'), 
-									"Born" => ociresult($s,'BORNDATE'), 
-									"Category" => ociresult($s,'NAIKAT'), 
+									"summa" => ociresult($s,'SUMMA'), 
+
 								);
 	
 			
 		} 
 		
-	print_r(utf8_json_encode($appoint_teac_json));
+	print_r(utf8_json_encode($stimul_teac_json));
  ?>
