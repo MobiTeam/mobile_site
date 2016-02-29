1918bda9c2a1200666305c08e27adad9303e9d99
Vladonxp <vladonxp@mail.ru>
2016-02-29 19:20:32 +0500

    anti brute

--- auth/ad_functions.php
+++ auth/ad_functions.php
@@ -12,6 +12,20 @@
            }
 }
   
+ 
+     function GetWordForm($n, $forms)
+      {
+          if ($n>0)
+          {
+              $n = abs($n) % 100;
+              $n1 = $n % 10;
+              if ($n > 10 && $n < 20) return $forms[2];
+              if ($n1 > 1 && $n1 < 5) return $forms[1];
+              if ($n1 == 1) return $forms[0];
+          }
+          return $forms[2];
+      }
+
   function auth($login, $password, $ad){ // функция авторизации на ldap сервере (логин нужно передавать полный)
 	        
 			global $data_user;
--- mobile_reciever.php
+++ mobile_reciever.php
@@ -1,12 +1,33 @@
 ﻿<?php 
     
 	if(isset($_POST['login'])){
-		
+
+
 		   require_once('auth/ad_settings.php');
 		   require_once('auth/ad_functions.php');
 		   require_once('auth/connecting_user.php');
-		   
-			$err_message = 'Ошибка авторизации: Неправильные логин или пароль';
+		   require_once('oracle/database_connect.php');
+
+		   modifyPost();
+
+		    $sql = "select 30 - (TO_CHAR(sysdate,'sssss') - TO_CHAR(DATE_INPUT,'sssss')) as LAST_TRYING from logs_users where login = '" . $_POST['login'] . "' and flag = 0 and DATE_INPUT > (sysdate - (30 / (24 * 60 * 60)))";
+		    $s = OCIParse($c,$sql);
+		    OCIExecute($s, OCI_DEFAULT); 
+
+		    while(OCIFetch($s)){
+				$cnt = ociresult($s,'LAST_TRYING');		
+			}
+
+			if(isset($cnt)){
+				
+				$data_user['FIO'] = 'undefined';	
+			  	$data_user['is_student'] = 'undefined';	
+			  	$data_user['serverRequest'] = 'Превышено число запросов. Повторная попытка ввода возможна через ' . $cnt . ' ' . GetWordForm($cnt, array('секунду', 'секунды', 'секунд')) . '.';
+
+				die(utf8_json_encode($data_user));
+			} 
+
+		    $err_message = 'Ошибка авторизации: Неправильные логин или пароль';
 			$data_message = 'Ошибка передачи авторизационных данных';
 			$succ_message = 'Успешная авторизация';
 			
@@ -42,11 +63,23 @@
 					  if(isset($serverRequest) && ($data_user['serverRequest'] == 'default')){
 						  $data_user['FIO'] = 'undefined';	
 						  $data_user['is_student'] = 'undefined';	
-						  $data_user['serverRequest'] = $serverRequest;				  
+						  $data_user['serverRequest'] = $serverRequest;
+
+						  $sql = "insert into logs_users (LOGIN, DATE_INPUT, IP_ADRESS, FLAG) values('" . $_POST['login'] . "', sysdate, '" . $_SERVER["REMOTE_ADDR"] . "', '0')";
+						  $s = OCIParse($c,$sql);
+						  OCIExecute($s, OCI_DEFAULT); 
+						  OCICommit($c); 	
+
 					  } else { // if ($data_user['serverRequest'] == 'default')
 						  $data_user['serverRequest'] = $succ_message;
 						  require_once('oracle/database_get_fullfio.php');				  
-						  require_once('oracle/database_update_users.php');				  
+						  require_once('oracle/database_update_users.php');	
+
+						  $sql = "insert into logs_users (LOGIN, DATE_INPUT, IP_ADRESS, FLAG) values('" . $_POST['login'] . "', sysdate, '" . $_SERVER["REMOTE_ADDR"] . "', '1')";
+						  $s = OCIParse($c,$sql);
+						  OCIExecute($s, OCI_DEFAULT); 
+						  OCICommit($c); 
+
 					  }
 
 					  print_r(utf8_json_encode($data_user));
